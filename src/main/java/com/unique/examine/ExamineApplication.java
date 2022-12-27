17d4a7253709aa5dfecd72eaa39bbfd0e4a655a4
ceshi <ceshi20160607@sina.com>
2022-12-27 14:31:25 +0800

    0.0基础框架

---
+++ src/main/java/com/unique/examine/ExamineApplication.java
@@ -0,0 +1,13 @@
+package com.unique.examine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unique/examine/ExamineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unique.examine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
