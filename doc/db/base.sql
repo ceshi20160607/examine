04db3c6c61e6ecf6eb302395ed64f694dc9d2208
ceshi <ceshi20160607@sina.com>
2023-02-10 17:41:00 +0800

    0.0.00001基础框架，基础主表定义

--- doc/db/base.sql
+++ doc/db/base.sql
@@ -4,7 +4,7 @@ CREATE TABLE `un_examine_group` (
 `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '分组名称',
 `create_user_id` bigint(20) DEFAULT NULL COMMENT '创建人',
 `update_user_id` bigint(20) DEFAULT NULL COMMENT '更新人',
-`create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+`create_time` datetime DEFAULT NULL COMMENT '创建时间',
 `update_time` datetime DEFAULT NULL COMMENT '更新时间',
 `company_id` bigint(20) DEFAULT NULL COMMENT '企业id',
 PRIMARY KEY (`id`) USING BTREE
@@ -19,25 +19,26 @@ CREATE TABLE `un_examine_apply_user` (
 
 `create_user_id` bigint(20) DEFAULT NULL COMMENT '创建人',
 `update_user_id` bigint(20) DEFAULT NULL COMMENT '更新人',
-`create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+`create_time` datetime DEFAULT NULL COMMENT '创建时间',
 `update_time` datetime DEFAULT NULL COMMENT '更新时间',
 `company_id` bigint(20) DEFAULT NULL COMMENT '企业id',
 PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='审批适用用户部门表';
 
-CREATE TABLE `un_examine_high_rule` (
+CREATE TABLE `un_examine_setting` (
 `id` bigint(20) NOT NULL COMMENT 'id',
 `examine_id` bigint(20) NOT NULL COMMENT '审批id',
 `rule_type` int(11) DEFAULT NULL COMMENT '0撤回规则 1通过规则 ',
 `recheck_type` int(11) DEFAULT NULL COMMENT '撤回之后重新审核操作 1 从第一层开始 2 从拒绝的层级开始',
+`pass_type` int(11) DEFAULT NULL COMMENT '通过规则类型 1 自动通过 2 超时通过',
 
-`apply_type` int(10) NOT NULL COMMENT '适用类型 0用户 1部门',
+`apply_type` int(10) NOT NULL COMMENT '适用类型 0默认 1用户 2部门',
 `user_id` bigint(20) NOT NULL COMMENT '适用用户id',
 `dept_id` bigint(20) NOT NULL COMMENT '适用部门id',
 
 `create_user_id` bigint(20) DEFAULT NULL COMMENT '创建人',
 `update_user_id` bigint(20) DEFAULT NULL COMMENT '更新人',
-`create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+`create_time` datetime DEFAULT NULL COMMENT '创建时间',
 `update_time` datetime DEFAULT NULL COMMENT '更新时间',
 `company_id` bigint(20) DEFAULT NULL COMMENT '企业id',
 PRIMARY KEY (`group_id`) USING BTREE
@@ -56,12 +57,14 @@ CREATE TABLE `un_examine` (
 -- `user_ids` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin COMMENT '可见范围（员工）',
 -- `dept_ids` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin COMMENT '可见范围（部门）',
 
-`batch_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '批次ID',
+-- `batch_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '批次ID',
 `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '备注',
 `create_time` datetime DEFAULT NULL COMMENT '创建时间',
 `create_user_id` bigint(20) DEFAULT NULL COMMENT '创建人',
-`update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+`update_time` datetime DEFAULT NULL COMMENT '修改时间',
 `update_user_id` bigint(20) DEFAULT NULL COMMENT '修改人',
 `company_id` bigint(20) DEFAULT NULL COMMENT '企业id',
 PRIMARY KEY (`id`) USING BTREE,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='审批表';
+
+
