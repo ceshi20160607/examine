b0266ef701ce4510a0068c3a6b1440f53f9fb88a
ceshi <ceshi20160607@sina.com>
2023-07-27 16:44:50 +0800

    0.0.00003 1.添加流程表结构

---
+++ src/main/java/com/unique/flow/service/IFlowFieldLogService.java
@@ -0,0 +1,16 @@
+package com.unique.flow.service;
+
+import com.unique.flow.entity.po.FlowFieldLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 流程关联表字段表 服务类
+ * </p>
+ *
+ * @author UNIQUE
+ * @since 2023-07-12
+ */
+public interface IFlowFieldLogService extends IService<FlowFieldLog> {
+
+}
---
+++ src/main/java/com/unique/flow/service/impl/FlowFieldLogServiceImpl.java
@@ -0,0 +1,20 @@
+package com.unique.flow.service.impl;
+
+import com.unique.flow.entity.po.FlowFieldLog;
+import com.unique.flow.mapper.FlowFieldLogMapper;
+import com.unique.flow.service.IFlowFieldLogService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 流程关联表字段表 服务实现类
+ * </p>
+ *
+ * @author UNIQUE
+ * @since 2023-07-12
+ */
+@Service
+public class FlowFieldLogServiceImpl extends ServiceImpl<FlowFieldLogMapper, FlowFieldLog> implements IFlowFieldLogService {
+
+}
