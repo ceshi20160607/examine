c71693dc12f9b3baf33f29e8821d3c5e3572cf55
ceshi <ceshi20160607@sina.com>
2023-12-28 16:32:56 +0800

    0.0.10014修正自定义模板

---
+++ core/src/main/resources/es/BaseService.java
@@ -0,0 +1,13 @@
+package com.kakarote.core.servlet;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * 通用service
+ *
+ * @param <T> model
+ * @author zhanzghiwei
+ */
+public interface BaseService<T> extends IService<T> {
+
+}
